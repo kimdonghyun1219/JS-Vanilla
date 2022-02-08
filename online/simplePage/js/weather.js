bb2e739b061b51124853b247b327ddaa46607ae1
KKKDH <84623138+kimdonghyun1219@users.noreply.github.com>
2022-02-08 23:39:02 +0900

    openweatherapi add
    URL: https://home.openweathermap.org/api_keys 
    get key

--- online/simplePage/js/weather.js
+++ online/simplePage/js/weather.js
@@ -1,7 +1,28 @@
 /** bring it the user geolocation  */
 
-function onGeoOk(){
+const API_KEY; // = "https://home.openweathermap.org/api_keys";
 
+
+function onGeoOk(position){
+    const lat = position.coords.latitude;        
+    const lng = position.coords.longitude;
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`;
+    fetch(url)
+        .then((response) => response.json())
+        .then((data) => {
+            const weather = document.querySelector("#weather span:first-child");
+            const city = document.querySelector("#weather span:last-child");
+            
+            const name = data.name;
+
+            city.innerText = name;
+            weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+    }); 
+    /**
+     *  fetch(): promise -> server가 응답할 때까지 시간소요. (즉시 발생하지 않음.)
+     *  then()을 사용
+     *  => URL을 fetch한 다음 response를 받음.
+     */
 }
 
 function onGeoError(){
