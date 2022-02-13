480bf697a5bd0fd94aee28769bedad4990c09233
KKKDH <84623138+kimdonghyun1219@users.noreply.github.com>
2022-02-13 23:37:58 +0900

    basic
    typeof, and, or

---
+++ online/grammar/es6/basic/basic.js
@@ -0,0 +1,43 @@
+/**
+ *   typeof
+ */
+   typeof 'good'; // String
+   typeof Symbol(); // symbol
+   typeof {}; // object
+   typeof []; // object
+   typeof true; // boolean
+   typeof(false); // boolean
+   typeof(123); // number
+   typeof(NaN); // number
+   typeof(123n); // bingint
+   typeof(undefined); // undefined
+   typeof null; // ** object
+   
+   function apple(){};
+   typeof apple; // function
+
+/**
+ *   AND, OR
+ * 
+ *   AND:
+ *      왼쪽값이 true면 오른쪽 값을 리턴, 왼쪽값이 false면 왼쪽값을 리턴.
+ *      console.log("good","apple"); => apple
+ *      console.log(true && false);  => false
+ *      console.log('0' && NaN); => NaN
+ *      console.log(null && undefined); => null
+ *      
+ *   OR:
+ *      왼쪽값이 true면 왼쪽값을 리턴, 왼쪽값이 false면 오른쪽값을 리턴.
+ *      console.log(0 || true);  => true
+ *      console.log({} || 123);  => {}
+ * 
+ *   우선순위: AND > OR
+ */   
+ 
+function print(value){
+  const message = value || 'good';
+  console.log(message);
+}
+
+print(); // good
+print('apple'); // apple
