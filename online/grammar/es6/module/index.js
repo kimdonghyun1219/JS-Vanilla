d13ee3b21d6d42c421076c9d0813074615c1faaa
KKKDH <84623138+kimdonghyun1219@users.noreply.github.com>
2022-02-24 23:29:24 +0900

    import export
    default

--- online/grammar/es6/module/index.js
+++ online/grammar/es6/module/index.js
@@ -1,9 +1,15 @@
 /** import {변수, 함수 ,...} from 모듈파일경로 */
 
+/** named import */
 import {title as printerTitle , print} from './printer.js'; // as를 사용하여 변수 변경
-import {title, data as members } from './members.js';
+import {title as atitle, data as members } from './members.js';
 
-
+/** 전부 불러오기 */
+// import * as printerJS from './printer.js';  -> default를 사용할 경우 printerJS.default 로 접근
 
 print(title);
 print(members);
+
+/** default import : as를 붙여 사용 */
+// 1. import {default as getArgu2} from './printer.js';
+// 2. import getArgu2, { title as printerTitle} from './printer.js'; -> default만 {}밖으로 빼서 분리가능.
--- online/grammar/es6/module/printer.js
+++ online/grammar/es6/module/printer.js
@@ -1,7 +1,19 @@
 /** export: 다른파일에서 사용할 수 있다. -> 단, 다른파일에서 모듈문법을 사용해야함.(import) */
 
+/** 선택적 export */
 export const title = 'very good';
 
 export function print(value){
     console.log(value);
 }
+
+const exportArgu1 = '1';
+const exportArgu2 = '2';
+const exportArgu3 = '3';
+
+/** export를 할 대상을 {}로 묶는방법.  */
+export {exportArgu1, exportArgu2 as Argu2, exportArgu3};
+
+
+/** default: 하나만 보낼 수 있음. -> 모듈 파일내에서 한번만 사용가능. */
+export default 'unique';
